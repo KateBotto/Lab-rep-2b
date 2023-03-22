5fcafccb57d3d8f796d96454b42c9c6b003d8aac
KateBotto <115860985+KateBotto@users.noreply.github.com>
2023-03-22 15:16:01 -0400

    Update 7segclass.ino

--- sevensegclass/7segclass.ino
+++ sevensegclass/7segclass.ino
@@ -1,22 +1,23 @@
 #include "Sevclass.h"
 
  int my7pins [7] {2,3,4,5,6,7,8};
- int mypins [7] {9,10,11,12,13,14,15};
+ //int mypins [7] {9,10,11,12,13,14,15};
 
 SevenDisplay my7(my7pins);
-SevenDisplay mydisplay(mypins);
+//SevenDisplay mydisplay(mypins);
 
 void setup() {
   // put your setup code here, to run once:
 my7.init();
-mydisplay.init ();
+//mydisplay.init ();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 for(int i = 0; i < 10; ++i){
   my7.display(i);
-  mydisplay.display(i);
+  //mydisplay.display(i);
   delay(1000);
   }
 }
+
